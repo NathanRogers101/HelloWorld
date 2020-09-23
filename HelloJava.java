f2fd2dbae04906fa19254363954535f7e86825a9
NathanRogers101 <39377996+NathanRogers101@users.noreply.github.com>
2020-09-23 12:11:33 -0700

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,23 @@
+//@Author Nathan Rogers
+
+public class HelloJava{
+		public static void main(String[]args){
+			System.out.println("Hello World!");
+			System.out.println("This is another Statement");			
+			int i = 0;
+			if (i == 0){
+				Left();
+			}
+			else
+			{
+				Right();
+			}
+				
+		}
+		public static void Left(){
+			System.out.println("Move left");
+		}
+		public static void Right(){
+			System.out.println("move Right");
+		}
+}
