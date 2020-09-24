89aef8647bd12559d61d645236c76984977060c8
NathanRogers101 <39377996+NathanRogers101@users.noreply.github.com>
2020-09-24 12:26:14 -0700

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+//@Author Nathan Rogers
+
+public class HelloJava{
+		public static void main(String[]args){
+			System.out.println("Hello World!");
+			System.out.println("This is another Statement");	
+		}
+}
